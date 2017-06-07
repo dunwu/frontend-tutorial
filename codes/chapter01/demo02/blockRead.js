c3e72604f78561213b3229fc34342b769b1ef4ce
victor zhang <atlantis1024@163.com>
2017-06-07 14:32:29 +0800

    :art: Improving structure / format of the code.

--- codes/chapter01/demo02/blockRead.js
+++
@@ -1,8 +0,0 @@
-/**
- * 阻塞式代码示例
- * Created by victor zhang on 2017/6/5.
- */
-var fs = require("fs");
-var data = fs.readFileSync('input.txt');
-console.log(data.toString());
-console.log("程序执行结束!");
