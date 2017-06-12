0d75b8896d9848e96d88e8d7952a4f0dfc5bdca9
victor zhang <atlantis1024@163.com>
2017-06-12 14:56:57 +0800

    :sparkles: 使用 colors 库为编译脚本的控制台信息着色

--- codes/chapter03/webpack2/buildAll.js
+++ codes/chapter03/webpack2/buildAll.js
@@ -1,30 +1,56 @@
 /**
  * Created by victor zhang on 2017/6/12.
  */
-const cp = require('child_process');
-const path = require("path");
-const fs = require("fs");
+const fs = require('fs');
+const path = require('path');
+const colors = require('colors/safe');
+const spawnSync = require('child_process').spawnSync;
 
-const cmds = fs.readdirSync(__dirname).filter(function (dirname) {
-  return fs.statSync(path.join(__dirname, dirname)).isDirectory() && dirname !== "node_modules";
-}).sort().map(function (dirname) {
-  return "cd " + dirname + " && webpack";
+const demoDirs = fs.readdirSync(__dirname).filter((file) => {
+  return fs.statSync(path.join(__dirname, file)).isDirectory()
 });
 
-let stack = function () {
-  console.log("done");
-};
-for (let i = cmds.length - 1; i >= 0; i--) {
-  const cmd = cmds[i];
-  stack = (function (next, cmd) {
-    return function () {
-      console.log(cmd);
-      cp.exec(cmd, function (error, stdout, stderr) {
-        if (error) console.error(error);
-        else if (stderr) console.error(stderr), next();
-        else next();
-      });
+let cmdArgs = [
+  { cmd: 'webpack', args: [] }
+];
+
+const build = function (demoDirs) {
+  for (const dir of demoDirs) {
+
+    // 跳过 node_modules 目录
+    if (dir === "node_modules") {
+      continue;
     }
-  }(stack, cmd));
-}
-stack();
+
+    // 打印开始编译信息
+    console.log(colors.green.bold.underline("Start building ") + colors.yellow.bold.underline(dir));
+
+    for (const cmdArg of cmdArgs) {
+      // declare opts in this scope to avoid https://github.com/joyent/node/issues/9158
+      const opts = {
+        cwd: path.join(__dirname, dir),
+        stdio: 'inherit',
+        encoding: 'utf8'
+      };
+
+      let result = {};
+      if (process.platform === 'win32') {
+        result = spawnSync(cmdArg.cmd + '.cmd', cmdArg.args, opts)
+      } else {
+        result = spawnSync(cmdArg.cmd, cmdArg.args, opts)
+      }
+
+      // 打印编译失败信息
+      if (result.status !== 0) {
+        console.log(colors.red.bold.underline("\nBuilding error.\n"));
+        return;
+      }
+    }
+  }
+
+  // 打印编译成功信息
+  console.log(colors.green.bold.underline("\nBuilding success..."));
+};
+
+build(demoDirs);
+
