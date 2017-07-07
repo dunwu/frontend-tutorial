8d062a8f7e5fae768c0aacf92d434fd84695b36e
Victor Zhang <atlantis1024@163.com>
2017-07-07 09:14:24 +0800

    :fire: 删除 antd-demo 示例

--- codes/antd-demo/app/components/About.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-class About extends React.PureComponent {
-  render() {
-    return <h2>关于</h2>
-  }
-}
-export default About;
--- codes/antd-demo/app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
-import '../assets/style/index.less';
-
-ReactDOM.render(
-  <Routes/>,
-  document.getElementById("root")
-);
