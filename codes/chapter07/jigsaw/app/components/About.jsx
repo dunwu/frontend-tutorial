536101abe866c327d7cc5b14697f39f5f6fcf7ee
Zhang Peng <atlantis1024@163.com>
2017-09-26 23:00:15 +0800

    :sparkles: 添加 chapter07 ESLint 示例

---
+++ codes/chapter07/jigsaw/app/components/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class About extends React.PureComponent {
+  render() {
+    return <h2>关于</h2>;
+  }
+}
---
+++ codes/chapter07/jigsaw/app/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../assets/style/index.css';
+import Routes from './routes';
+
+ReactDOM.render(
+  <Routes />,
+  document.getElementById('root')
+);
